d23401035b3d25ac3c5809f7eeef33f555d4ca6d
Connor-woodahl <Connor_woodahl1@baylor.edu>
2020-05-26 14:49:11 -0700

    Added comments for plans when I have functions to call

--- Server/webserver.py
+++ Server/webserver.py
@@ -23,16 +23,39 @@ books = [
      'published': '1975'}
 ]
 
-
 @app.route('/', methods=['GET'])
 def home():
-    return '''<h1>Team A Server</h1>
-<p>Home page of Team A to parse log files. To test enter the path "/api/test"</p>'''
-
+    return '''<h1><center>Team A Server</center></h1>
+<p>Home page of Team A to parse log files.</p>
+<p> To test enter the path "/api/test".</p>
+<p> To parse the example given by Red Hat use "/logs/default".</p>
+<p> To parse a folder use "/logs/folder" with the path to that folder as an argument.</p>'''
 
 # A route to return all of the available entries in our catalog.
 @app.route('/api/test', methods=['GET'])
 def api_all():
     return jsonify(books)
+
+# A route to return JSON for log files in given path
+# This will call the pipeline function for all pipeline.log files found and will call the aggregate
+# function for all aggregate.log files found in the path and return results to the user
+@app.route('/logs/folder', methods=['POST'])
+def post_logs(path):
+    #Search Path here
+        #If pipeline.log call pipeline function written by Jackson
+        #If aggregate.log call function written by Brooklynn. 
+        #Append the result of these calls to the JSON object to return to the user
+    #Return error if unable to find or open path or files 
+    #return JSON Object
+    return jsonify(books)
+
+
+# A route to return JSON for default log files
+# This will call some combo of our pipeline and aggregate functions to return the proper json output
+# from the logs file given to us by Red Hat
+@app.route('/logs/default', methods=['GET'])
+def get_logs():
+    return jsonify(books)
+
 
 app.run()

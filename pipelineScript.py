ac8479eed505083c9314c7deba41d3615c793c2f
brooklynnStone1 <stone.brooklynn@gmail.com>
2020-05-27 11:58:28 -0500

    Fixed parsing issues and added time stamp

--- pipelineScript.py
+++ pipelineScript.py
@@ -2,14 +2,23 @@ import json
 
 # Class to hold desired information about each log
 class LogItem:
+    orgId = -1
+    clusterName = ""
+    partition = -1
+    offset = -1
+    timestamp = ""
+    isError = False
+    isWarning = False
+    messages = []
     def _init_(self):
         self.orgId = -1
         self.clusterName = ''
         self.partition = -1
         self.offset = -1
+        self.timestamp = ""
         self.isError = False
         self.isWarning = False
-        self.messages = {}
+        self.messages = []
 
 # Verify if a line is in JSON format
 # @params myjson: the object to verify json form of
@@ -46,22 +55,28 @@ def get_log_items(pipeline_path):
     for i in range(len(logs)):
         # Beginning of a chunk of data, so save its message
         if logs[i]['levelname'] == "INFO":
-            if len(msgArr) == 0:
-                msgArr.append(logs[i]['message'])
-            else:
-                del msgArr[:]
+            del msgArr[:]
+            msgArr.append(logs[i]['message'])
         else:
             # If at the end of the chunk, save orgId, cluster name, partition, and offset
-            if logs[i]['message'].count("OrgId") == 1:
+            if "OrgId" in logs[i]['message']:
                 message = logs[i]['message'].split(',')
                 item = LogItem()
                 item.orgId = message[0][message[0].find("=")+1:]
-                item.clusterName = message[0][message[0].find("=")+2:-1]
-                item.partition = msgArr[0][msgArr[0].find("Partition")+11:msgArr[0].find("Offset")-3]
-                item.offset = msgArr[0][msgArr[0].find("Offset")+8:msgArr[0].find("LastChecked")-3]
-                item.messages = msgArr
+                item.clusterName = message[1][message[1].find("=")+2:-1]
+
+                message = msgArr[0].split(';')
+                if "Partition" in msgArr[0]:
+                    item.partition = message[2][11:]
+                if "Offset" in msgArr[0]:
+                    item.offset = message[3][9:]
+
+                item.messages = [None]*len(msgArr)
+                for i in range(len(msgArr)):
+                    item.messages[i]= msgArr[i]
                 item.isError = isError
                 item.isWarning = isWarning
+                item.timestamp = logs[i]['asctime']
                 logItems.append(item)
                 del msgArr[:]
                 del item
@@ -79,5 +94,6 @@ def get_log_items(pipeline_path):
 
 logs = get_log_items("logs/pipeline.log")
 for i in range(len(logs)):
-    print(logs[i].orgId, logs[i].clusterName, logs[i].messages)
+    print(logs[i].orgId, logs[i].clusterName, logs[i].partition, logs[i].offset, "Is Error?", logs[i].isError, logs[i].timestamp)
+    print(logs[i].messages, "\n")
         

4b56c2fde07a5d14c1246cccd8a23121d8b63f7d
JacksonRaffety <jwraffety@gmail.com>
2020-05-28 10:53:33 -0700

    fixed search by offset

--- searchscript.py
+++ searchscript.py
@@ -3,15 +3,11 @@ import sys
 
 
 def search_by_offset(path_to_log, offset):
-    log_list = aggregatorscript.get_log_list(path_to_log)
-
-    groups = aggregatorscript.get_groups(log_list)
-
+    groups = aggregatorscript.get_groups(path_to_log)
     return [group for group in groups if group.offset == offset]
 
 
 if __name__ == "__main__":
-    print(sys.argv[1])
-    logs = aggregatorscript.get_log_list("logs/aggregator.log")
-    results = search_by_offset(sys.argv[1], 1055)
-    print(results)
+    results = search_by_offset("logs/aggregator.log", 1055)
+    for result in results:
+        print(result.offset)

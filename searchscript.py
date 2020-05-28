085f457df00cef0417819600faa7478077f687e6
JacksonRaffety <jwraffety@gmail.com>
2020-05-28 10:39:29 -0700

    fixed formatting in searchscript

--- searchscript.py
+++ searchscript.py
@@ -1,17 +1,17 @@
-import aggregatorScript
+import aggregatorscript
 import sys
 
 
-def searchByOffset(path_to_log, offset):
-    log_list = aggregatorScript.get_log_list(path_to_log)
+def search_by_offset(path_to_log, offset):
+    log_list = aggregatorscript.get_log_list(path_to_log)
 
-    groups = aggregatorScript.get_groups(log_list)
+    groups = aggregatorscript.get_groups(log_list)
 
     return [group for group in groups if group.offset == offset]
 
 
 if __name__ == "__main__":
     print(sys.argv[1])
-    logs = aggregatorScript.get_log_list("logs/aggregator.log")
-    results = searchByOffset(sys.argv[1], 1055)
+    logs = aggregatorscript.get_log_list("logs/aggregator.log")
+    results = search_by_offset(sys.argv[1], 1055)
     print(results)

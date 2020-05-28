cdbe3ba58e204bc673f331960f2142095b6e5048
Jan Svacina <jan@svacina.net>
2020-05-28 09:27:17 -0500

    requirements file

--- tester.py
+++
@@ -1,8 +0,0 @@
-import fnmatch
-import os
-
-dirpath='./logs'
-
-for file in os.listdir('./logs'):
-    if fnmatch.fnmatch(file, 'aggregator*.log'):
-        print(os.path.abspath(os.path.join(dirpath, file)))

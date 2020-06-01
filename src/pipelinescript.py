5c229a2ca90ee300641f450e671ccdce1fdfe4ab
Connor-woodahl <Connor_woodahl1@baylor.edu>
2020-06-01 08:45:43 -0700

    Added connector, filesystem, and new structure

---
+++ src/pipelinescript.py
@@ -0,0 +1,148 @@
+import json
+
+
+# Class to hold desired information about each log
+class LogItem:
+
+    def _init_(self):
+        orgId = -1
+        clusterName = ""
+        partition = -1
+        offset = -1
+        timestamp = ""
+        error = False
+        warning = False
+        messages = []
+
+
+# Verify if a line is in JSON format
+# @params myjson: the object to verify json form of
+# @return boolean indicating whether the object can be parsed into json
+def is_json(myjson):
+    try:
+        json_object = json.loads(myjson)
+    except ValueError as e:
+        return False
+    return True
+
+
+# Extracts lines from log file in json format
+# @params pipeline_path: the file to extract the logs from
+# @return array of all the logs as json objects
+def get_log_list(pipeline_path):
+    log_list = [{}]
+
+    for line in open(pipeline_path, encoding='utf-8'):
+        if is_json(line):
+            log_list.append(json.loads(line))
+
+    return log_list[2:]
+
+
+# Reads through logs and assembles an array of LogItem objects
+# @params pipeline_path: the path to the pipeline log
+# @return an array of LogItem objects as JSON objects
+def get_log_items(pipeline_path):
+    logItems = []
+    logs = get_log_list(pipeline_path)
+    isError = False
+    isWarning = False
+    msgArr = []
+    partition = -1
+    offset = -1
+    info_message = ""
+    # Loop through each line of the logs and grab desired information
+    for i in range(len(logs)):
+        # Beginning of a chunk of data, so save its message
+        if logs[i]['levelname'] == "INFO":
+            # If the previous block had messages associated with it
+            if len(msgArr) > 1:
+                item = LogItem()
+                # Check for orgId and clusterName
+                if "OrgId" in logs[i - 1]['message'] or "ClusterName" in logs[i - 1]['message']:
+                    message = logs[i - 1]['message'].split(',')
+                    for h in range(len(message)):
+                        if "OrgId" in message[h]:
+                            item.orgId = message[h][message[h].find("=") + 1:]
+                        elif "ClusterName" in message[h]:
+                            item.clusterName = message[h][message[h].find("=") + 2:-1]
+
+                # check for partition and offset
+                if "Partition" in msgArr[0] or "Offset" in msgArr[0]:
+                    message = msgArr[0].split(';')
+                    for k in range(len(message)):
+                        if "Partition" in message[k]:
+                            partition = message[k][12:]
+                        elif "Offset" in message[k]:
+                            offset = message[k][9:]
+                item.partition = partition
+                item.offset = offset
+
+                # Assign message array and error/warning members
+                item.messages = [None] * len(msgArr)
+                for j in range(len(msgArr)):
+                    item.messages[j] = msgArr[j]
+                item.error = isError
+                item.warning = isWarning
+                item.timestamp = logs[i - 1]['asctime']
+                logItems.append(item)
+                del item
+                isError = False
+                isWarning = False
+                partition = -1
+                offset = -1
+            del msgArr[:]
+        else:
+            if "ERROR" in logs[i]['levelname']:
+                isError = True
+            elif "WARNING" in logs[i]['levelname']:
+                isWarning = True
+        msgArr.append(logs[i]['message'])
+
+    # Append final log item
+    if len(msgArr) > 1:
+        item = LogItem()
+        # Check for orgId and clusterName
+        if "OrgId" in logs[len(logs) - 1]['message'] or "ClusterName" in logs[len(logs) - 1]['message']:
+            message = logs[len(logs) - 1]['message'].split(',')
+            for h in range(len(message)):
+                if "OrgId" in message[i]:
+                    item.orgId = message[h][message[h].find("=") + 1:]
+                elif "ClusterName" in message[h]:
+                    item.clusterName = message[h][message[h].find("=") + 2:-1]
+        # check for partition and offset
+        if "Partition" in msgArr[0] or "Offset" in msgArr[0]:
+            message = msgArr[0].split(';')
+            print(message)
+            for j in range(len(message)):
+                if "Partition" in message[j]:
+                    item.partition = message[j][11:]
+                elif "Offset" in message[i]:
+                    item.offset = message[j][9:]
+
+        # Assign message array and error/warning members
+        item.messages = [None] * len(msgArr)
+        for i in range(len(msgArr)):
+            item.messages[i] = msgArr[i]
+        item.error = isError
+        item.warning = isWarning
+        item.timestamp = logs[i - 1]['asctime']
+        logItems.append(item)
+        del msgArr[:]
+        del item
+        isError = False
+        isWarning = False
+        partition = -1
+        offset = -1
+
+    # Convert objects to json objects
+    json_logs = []
+    for i in range(len(logItems)):
+        json_logs.append(logItems[i].__dict__)
+    return json_logs
+
+
+# If you want to test with command line, input file name here
+# logs = get_log_items("logs/pipeline.log")
+# for i in range(len(logs)):
+#     print(logs[i])

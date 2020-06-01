1f5d51001df1afe56aa3ade940237b8f373cbfb4
JetBox <zeldagameremail@gmail.com>
2020-06-01 11:31:56 -0700

    updated offset searching to match requirements

--- src/clusteranalysis.py
+++ src/clusteranalysis.py
@@ -1,9 +1,14 @@
-from src import aggregatorscript
+from src import aggregatorscript, pipelinescript
 import sys
 
 
 
-
-
-if __name__ == "__main__":
-    sys.exit(1)
+# @param path_to_log: log file to read the logs from
+# @param organization: the organization id to search by
+# @param cluster_id: the id of the cluster to search by
+# @return value: list of ConsumedGroups found in log file that match the given organization and cluster_id
+def get_results_by_org_cluster(path_to_log, organization, cluster_id):
+    groups = aggregatorscript.get_groups(path_to_log)
+    groups.extend(pipelinescript.get_log_items(path_to_log))
+    cluster_matches = [group for group in groups if group.cluster_id == cluster_id]
+    matches = [matches for matches in cluster_matches if matches.organization == organization]

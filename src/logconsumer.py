f110de0acd5ed382fa2a05ca1dcfc57f79f86adf
Jan Svacina <jan@svacina.net>
2020-06-03 00:23:08 -0500

    log producer and consumer

---
+++ src/logconsumer.py
@@ -0,0 +1,5 @@
+from kafka import KafkaConsumer
+from json import loads
+consumer = KafkaConsumer('test', auto_offset_reset='earliest',enable_auto_commit=True, bootstrap_servers='localhost:9092')
+for msg in consumer:
+        print (msg)

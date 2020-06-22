e373e940e357ce070b0010733437e9fbc660576c
Connor-woodahl <Connor_woodahl1@baylor.edu>
2020-06-22 13:36:25 -0700

    Documentation and comments

---
+++ src/logtax.py
@@ -0,0 +1,14 @@
+# Input: list of log files (perhaps only 2 for the prototype)
+# Output: list of log classifications
+# Process: First, assume log files are in JSON format, or are easily convertible to dictionary.
+# For our example logs use the pre-existing scripts, but for other log examples,
+# use a more basic process. After classify the log messages by a pre-determined list of log types
+def multi_log_match(logs: list):
+    results = []
+    # step 1: data validation
+
+    # step 2: process data
+
+    # step 3: classify
+
+    return results
